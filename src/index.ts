1f59b2807a98ad408d429c9bfd014e42387ede5f
Clemens Akens <clebert@me.com>
2020-01-15 14:49:34 +0100

    Reimplement useReducer Hook

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './runner';
 export * from './use-callback';
 export * from './use-effect';
 export * from './use-memo';
+export * from './use-reducer';
 export * from './use-ref';
 export * from './use-state';
